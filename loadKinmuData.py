a5bb39e1bf2927f2f4a2a6960ff189dcc60ee020
dulaiming <dulaiming@gmail.com>
2021-01-23 14:02:47 +0900

    load csv from url

--- loadKinmuData.py
+++ loadKinmuData.py
@@ -4,6 +4,7 @@ import pandas
 import time
 current_milli_time = lambda: int(round(time.time() * 1000))
 import properties as pp
+from pathlib import Path
 
 def loadCsvFromUrl(id, date):
   url = pp.PP['url_appspot_prefix'] +  id +  pp.PP['url_appspot_suffix'] + date + "/" + id + "?" + str(current_milli_time())
@@ -11,6 +12,13 @@ def loadCsvFromUrl(id, date):
   data = pandas.read_csv(url, names=names)
   return data
 
+def saveCsvFromUrl(id, date):
+  url = pp.PP['url_appspot_prefix'] +  id +  pp.PP['url_appspot_suffix'] + date + "/" + id + "?" + str(current_milli_time())
+  names = ['id', 'date', 'startTime', 'endTime', 'userAgent', 'ipAddress', 'ua', 'ip']
+  data = pandas.read_csv(url, names=names)
+  data.to_csv(Path(__file__).parent.absolute().as_posix() + "/kinmu.csv")
+  print(Path(__file__).parent.absolute().as_posix() + "/kinmu.csv")
+  
 
 
 
